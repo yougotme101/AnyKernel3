1d7fd2c9c6fb21f51b34c61c7cbd0600bd654d03
yougotme101 <raflynaxrocker@gmail.com>
2025-12-25 15:05:03 +0700

    Update kernel string in anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=by yougotme @ github
+kernel.string=by yougotme101 @ github
 do.devicecheck=1
 do.modules=0
 do.systemless=0
