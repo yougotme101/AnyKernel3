33c76848f4996f17051fbd3f8e7939cb17e4560c
yougotme101 <raflynaxrocker@gmail.com>
2026-07-18 13:19:45 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -16,7 +16,7 @@ device.name3=sapphiren
 device.name4=sapphire
 device.name5=xun
 device.name6=creek
-supported.versions=13-16
+supported.versions=13-17
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
